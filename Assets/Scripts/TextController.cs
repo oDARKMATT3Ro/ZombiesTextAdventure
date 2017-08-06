4380e4ce181da0845154aa67800fc249e9f58cc0
Geoff GoeresHill <geoff.goeres@gmail.com>
2017-08-06 11:15:04 -0700

    Added start game and text placeholder

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -9,13 +9,19 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		text.text = "";
+		text.text = "Press SPACE to Start Game";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			text.text = "Space Key Pressed";
+			text.text = "Lorem ipsum dolor sit amet, consectetur " +
+						"adipiscing elit. Fusce cursus tristique magna. " +
+						"Nullam aliquet tellus a commodo sagittis. Donec " +
+						"mattis condimentum purus, vel elementum lacus " +
+						"pretium id. Nam nec dolor risus. Suspendisse potenti. " +
+						"Vestibulum nec aliquam ante. Sed magna turpis, varius " +
+						"et pellentesque pretium, fringilla id orci.";
 		}
 	}
 }
